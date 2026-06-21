5be2c070afa5a6de394d6f030513a8edf98588f1
rodrigolcschulz <rodrigolcschulz@outlook.com>
2026-06-21 17:55:41 -0300

    fix: restore Tailwind styles on mobile and improve LAN dev server config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    'http://localhost:3088',
+    'http://127.0.0.1:3088',
+    'http://192.168.1.11:3088',
+  ],
 };
 
 export default nextConfig;
